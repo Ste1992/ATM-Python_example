348eb94ef3df03c64668cde6ea72dbc4298b3cb4
Stefano <141121440+Ste1992@users.noreply.github.com>
2023-08-14 10:57:17 +0200

    login.py

--- login.py
+++ login.py
@@ -1,9 +1,25 @@
 class Login:
 
-    def __init__(self, nome, cognome, email, password, saldo, iban):
-        self.nome = nome
+    def __init__(self, cognome, nome, email, password, saldo, iban):
         self.cognome = cognome
+        self.nome = nome
         self.email = email
         self.password = password
         self.saldo = saldo
-        self.iban = iban
+        self.iban_account = iban
+
+    def get_attribute_by_column_name(self, column_name):
+        if column_name == 'Cognome':
+            return self.cognome
+        elif column_name == 'Nome':
+            return self.nome
+        elif column_name == 'Email':
+            return self.email
+        elif column_name == 'Password':
+            return self.password
+        elif column_name == 'Saldo':
+            return self.saldo
+        elif column_name == 'IBAN':
+            return self.iban_account
+        else:
+            raise KeyError(f"Column '{column_name}' not found")
