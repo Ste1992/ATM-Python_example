b9819cc3653a20baf5ca1df9ef1c0587e6af4d2a
Stefano <141121440+Ste1992@users.noreply.github.com>
2023-08-18 12:19:32 +0200

    correntista.py

--- correntista.py
+++ correntista.py
@@ -1,15 +1,17 @@
 import random
-import csv
+import string
+from account_database import LoginDatabase
+
 from login import Login  # importa la classe Login dal file login.py
 
 
 class Correntista:
+    database = LoginDatabase()
 
     def __init__(self):  # Initilizing the variables
         self.login = Login("cognome", "nome", "email", "password", 0.0, "IBAN")
-        self.accounts = set()  # Accounts list variable
-        self.account_loggato = None  # Logged in account
-        self.account = set()  # Signed up accounts list
+        self.account = None  # Logged in account
+        self.account_loggato = None
         self.nome = None  # User's name
         self.cognome = None  # User's surname
         self.email = None
@@ -20,9 +22,10 @@ class Correntista:
         self.cognome_ricevente = None  # Receiver's surname
         self.iban_ricevente = None  # Receiver's IBAN
 
+    # Iban generator method
     def iban_generator(self):
-        letters = "ABCDEFGHILKMNOPQRSTUWVUXYZ"  # Letters' string
-        numbers = "0123456789"  # Numbers' string
+        letters = string.ascii_letters.upper()  # Print letters upper case
+        numbers = string.digits  # Print numbers
         # Generate a 12 string casual characters: (8 letters, 2 numbers)
         random_part = random.choices(letters, k=10) + random.choices(numbers, k=2)
         nuovo_iban = "IT" + ''.join(random_part)  # Adds the "IT" prefix at the random_part
@@ -32,16 +35,40 @@ class Correntista:
         print("Benvenuto in Fuckbank!\nSeleziona dal menù:\n\t1. Registrati\n\t2. Login")
 
     def registrazione_utente(self):  # Sign up method
+        self.database.init_db()
+
         print("Registra un nuovo account\n")
         self.nome = input("Nome: ")
         self.cognome = input("Cognome: ")
-        self.email = input("Email: ")
+
+        # Create and handle email
+        email_username = input("Username: ")
+        while True:
+            try:
+                email_dominio = int(input('''Scegli il dominio:
+                1- @google.com
+                2- @outlook.com
+                3- @libero.it
+                '''))
+                break
+            except ValueError:
+                print("Inserisci un valore valido.")
+                continue
+
+        dominio = ["@google.com", "@outlook.com", "@libero.it"]
+        if email_dominio == 1:
+            self.email = email_username + dominio[0]
+        elif email_dominio == 2:
+            self.email = email_username + dominio[1]
+        elif email_dominio == 3:
+            self.email = email_username + dominio[2]
+
         self.password = input("Password: ")
 
         nuovo_iban = self.iban_generator()
 
         # Checks the user email with all the registered mails. If a match is found it asks to try with another one.
-        email_esistente = any(account.email == self.email for account in self.accounts)
+        email_esistente = self.database.user_collection.find_one({'email': self.email}) is not None
         if email_esistente:
             print("Email già registrata. Riprova.")
         else:  # Otherwise the account is correctly registered
@@ -53,23 +80,36 @@ class Correntista:
             Password: {self.password}
             Saldo: {self.saldo}
             IBAN: {nuovo_iban}""")
-            nuovo_account = Login(self.cognome, self.nome, self.email, self.password, 0.0, nuovo_iban)
-            self.accounts.add(nuovo_account)  # Add the new account to the accounts' list
+
+            # Creates a Login object using the user's data
+            nuovo_account = {"nome": self.nome, "cognome": self.cognome, "email": self.email, "password": self.password,
+                             "saldo": self.saldo, "iban": nuovo_iban}
+
+            # Inserts the Dict into the Database
+            insert_result = self.database.user_collection.insert_one(nuovo_account)
+
+            if insert_result.inserted_id:
+                print("Account inserito con successo!")
+            else:
+                print("Errore durante l'inserimento dell'account.")
 
     def login_utente(self):  # User login method
-        print("Effettua il login\n")
+        self.database.init_db()
 
-        while self.account_loggato is None:  # Asks for email and password
+        print("Effettua il login\n")
+        while True:  # Asks for email and password
             self.email = input("Email: ")
             self.password = input("Password: ")
 
-            for account in self.accounts:  # Checks through all the accounts until it acquires the required account data
-                if self.email == account.email and self.password == account.password:  # If they matches
-                    print(f"Benvenuto {account.nome} {account.cognome}")  # Welcome message: name + surname
-                    self.account_loggato = account  # Aggiungi l'account loggato a self.login_loggato
-                    break
+            self.account_loggato = self.database.user_collection.find_one({"email": self.email, "password": self.password})
 
-            if self.account_loggato is None:  # If the email and password don't match any of the accounts
+            if self.account_loggato is not None:  # If they matches
+                self.nome = self.account_loggato['nome']
+                self.cognome = self.account_loggato['cognome']
+                print(f"Benvenuto {self.nome} {self.cognome}")  # Welcome message: name + surname
+                break
+
+            else:  # If the email and password don't match any of the accounts
                 scelta = input("""Credenziali errate! Riprova o registrati:
                 \t1. Riprova
                 \t2. Registrati
@@ -81,22 +121,8 @@ class Correntista:
                     self.registrazione_utente()
                     break
 
-    def prelievo(self):  # Withdrawal method
-        while True:
-            try:  # Checks whether the input is numeric
-                importo = float(input("Inserire l'importo da prelevare: €"))
-                break
-            except ValueError:
-                print("Inserisci un valore valido.")
-                continue
-
-        if importo <= self.account_loggato.saldo:  # If it's less than the balance, show the result
-            self.account_loggato.saldo -= importo
-            print(f"\nPrelievo di €{importo} effettuato.\nNuovo saldo: €{self.account_loggato.saldo}\n")
-        else:  # If it's more, it fails
-            print("Importo non valido o saldo insufficiente.")
-
     def deposito(self):  # Deposit method
+        self.database.init_db()
         while True:
             try:  # Checks whether the input is numeric
                 importo = float(input("Inserire l'importo da depositare: €"))
@@ -105,16 +131,52 @@ class Correntista:
                 print("Inserisci un valore valido.")
                 continue
 
-        self.account_loggato.saldo += importo
-        print(f"Saldo corrente: €{self.account_loggato.saldo}")
+        nuovo_saldo = self.account_loggato['saldo'] + importo
+
+        self.database.user_collection.update_one(
+            {"_id": self.account_loggato['_id']},
+            {'$set': {'saldo': nuovo_saldo}}
+        )
+
+        self.account_loggato['saldo'] = nuovo_saldo
+
+        print(f"Saldo corrente: €{nuovo_saldo}")
+
+    def prelievo(self):  # Withdrawal method
+        self.database.init_db()
+
+        while True:
+            try:  # Checks whether the input is numeric
+                importo = float(input("Inserire l'importo da prelevare: €"))
+                break
+            except ValueError:
+                print("Inserisci un valore valido.")
+                continue
+
+        if importo <= self.account_loggato['saldo']:  # If it's less than the balance, show the result
+
+            nuovo_saldo = self.account_loggato['saldo'] - importo
+
+            self.database.user_collection.update_one(
+                {"_id": self.account_loggato['_id']},
+                {"$set": {'saldo': nuovo_saldo}}
+            )
+
+            self.account_loggato['saldo'] = nuovo_saldo
+
+            print(f"\nPrelievo di €{importo} effettuato.\nNuovo saldo: €{nuovo_saldo}\n")
+        else:  # If it's more, it fails
+            print("Importo non valido o saldo insufficiente.")
 
     def bonifico(self):  # Bank transfer method
         bonifico_ok = True  # Checks whether the bank transfer is successful
+
         while bonifico_ok:
-            print(f"Il tuo saldo è: €{self.account_loggato.saldo}")
+            print(f"Il tuo saldo è: €{self.account_loggato['saldo']}")
             nome_ricevente = input("Nome del ricevente: ")
             cognome_ricevente = input("Cognome del ricevente: ")
             iban_ricevente = input("IBAN: ")
+
             while True:
                 try:  # Checks whether the input is numeric
                     importo = float(input("Inserisci l'importo: €"))
@@ -123,79 +185,46 @@ class Correntista:
                     print("Inserisci un valore valido.")
                     continue
 
-            ricevente_trovato = False  # Checks whether the receiver is found
-            account_ricevente = None  # Stores the receiver's data account
+            account_ricevente = self.database.user_collection.find_one({
+                'nome': nome_ricevente,
+                'cognome': cognome_ricevente,
+                'iban': iban_ricevente
+            })
 
-            for account in self.accounts:
-                if (nome_ricevente == account.nome and
-                        cognome_ricevente == account.cognome and
-                        iban_ricevente == account.iban):
-                    ricevente_trovato = True  # The inputs match an account datas
-                    account_ricevente = account  # Initializes the variable
-                    break
+            if account_ricevente is not None:
+                if importo <= self.account_loggato['saldo']:  # If the amount to send is less than the balance
+                    saldo_utente_loggato = self.account_loggato['saldo'] - importo
+                    saldo_ricevente = account_ricevente['saldo'] + importo
+                    self.account_loggato['saldo'] = saldo_utente_loggato
+                    account_ricevente['saldo'] = saldo_ricevente
 
-            if ricevente_trovato:  # Elaborates the transfer and shows the result
-                if importo <= self.account_loggato.saldo:  # If the amount to send is less than the balance
-                    saldo_utente = self.account_loggato.saldo - importo
-                    saldo_ricevente = account.saldo + importo
-                    self.account_loggato.saldo = saldo_utente
-                    account_ricevente.saldo = saldo_ricevente
-                    print(f"Il saldo corrente è: €{saldo_utente}")  # Current user balance
+                    self.database.user_collection.update_one(
+                        {'_id': self.account_loggato['_id']},
+                        {"$set": {"saldo": saldo_utente_loggato}}
+                    )
+
+                    self.database.user_collection.update_one(
+                        {'_id': account_ricevente['_id']},
+                        {"$set": {"saldo": saldo_ricevente}}
+                    )
+
+                    self.account_loggato['saldo'] = saldo_utente_loggato
+                    account_ricevente['saldo'] = saldo_ricevente
+
+                    print(f"Il saldo corrente è: €{saldo_utente_loggato}")  # Current user balance
                     print("Bonifico effettuato con successo!")
                     print(f"\nNuovo saldo ricevente: €{saldo_ricevente}")  # Current receiver's balance
                     bonifico_ok = True  # The bank transfer is successful
-                elif importo > self.account_loggato.saldo:  # If the amount is more than the balance
+
+                else:  # If the amount is more than the balance
                     print("L'importo supera il saldo disponibile!")
                     bonifico_ok = False  # The bank transfer fails
                     continue
-                else:  # If the inputs don't match any of the accounts
-                    print("Dati inseriti errati!")
-                    bonifico_ok = False  # The bank transfer fails
-                    continue
 
-    def save_data_to_csv(self, filename):  # Save data method: it takes the file name as parameter
-        with open(filename, 'w', newline='') as csvfile:  # Opens, writes the file as a .csv file extension
-            fieldnames = ['Cognome', 'Nome', 'Email', 'Password', 'Saldo', 'IBAN']
-
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            # L'oggetto csv.DictWriter userà questo file per scrivere i dati.
-            # fieldnames: Questi nomi di colonna corrispondono alle chiavi dei dizionari che verranno passati a writerow()
-            # per scrivere una riga nel file CSV.
-
-            writer.writeheader()  # Writes the header in the CSV file: it uses the columns' names defined using "DictWriter"
-
-            for account in self.accounts:
-
-                if account.iban_account is None:
-                    iban_to_use = self.nuovo_iban
-                else:
-                    iban_to_use = account.iban_account
-
-                row = {
-                    'Cognome': account.cognome,
-                    'Nome': account.nome,
-                    'Email': account.email,
-                    'Password': account.password,
-                    'Saldo': str(account.saldo),
-                    'IBAN': iban_to_use
-                }
-                row_str = ", ".join(row.values())
-                csvfile.write(row_str + '\n')
-
-    def load_data_from_csv(self, filename):
-        self.accounts.clear()  # Clear existing accounts before loading
-
-        with open(filename, 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                self.accounts.add(Login(
-                    row['Cognome'],
-                    row['Nome'],
-                    row['Email'],
-                    row['Password'],
-                    float(row['Saldo']),
-                    row['IBAN']
-                ))
+            else:  # If the inputs don't match any of the accounts
+                print("Dati inseriti errati!")
+                bonifico_ok = False  # The bank transfer fails
+                continue
 
     def logout(self):  # Logout method
         scelta = input("Sei sicuro di voler uscire? Y/N ").casefold()  # Asks the user if to logout
@@ -208,4 +237,5 @@ class Correntista:
             else:  # Error message if the answer is not "y" or "n"
                 print("Scelta errata!")
                 continue
+        self.database.close_db()
 
