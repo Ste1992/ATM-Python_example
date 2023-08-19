4b4799eded41bea16e7fb5f495bb5bb19062231c
Stefano <141121440+Ste1992@users.noreply.github.com>
2023-08-19 23:16:56 +0200

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-class Login:
-
-    def __init__(self, cognome, nome, email, password, saldo, iban):
-        self.cognome = cognome
-        self.nome = nome
-        self.email = email
-        self.password = password
-        self.saldo = saldo
-        self.iban_account = iban
